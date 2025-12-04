1210451c62645ca6ba7f3f531e39305d8fdac8e7
allychu <allychu@umail.ucsb.edu>
2025-12-03 21:51:28 -0800

    added image uploading and resizing

--- projects/finalproject/sketch.js
+++ projects/finalproject/sketch.js
@@ -14,6 +14,7 @@ let drawBtn;
 let stickersBtn;
 let changeBgBtn;
 let colorPicker;
+let imageInput; // hidden file input
 
 // Scrapbook area variables
 let scrapbookX;
@@ -26,13 +27,19 @@ let scrapbookBgColor;
 let textElements = [];
 let selectedText = null;
 
+// Image elements array
+let imageElements = [];
+let selectedImage = null;
+
 // Typing mode variables
 let typingMode = false;
 let currentText = '';
 let typingX = 0;
 let typingY = 0;
 
+// =====================
 // TextElement class
+// =====================
 class TextElement {
   constructor(content, x, y, size, color) {
     this.content = content;
@@ -85,6 +92,92 @@ class TextElement {
   }
 }
 
+// =====================
+// ImageElement class
+// =====================
+class ImageElement {
+  constructor(img, x, y, w, h) {
+    this.img = img;
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.dragging = false;
+    this.resizing = false;
+    this.offsetX = 0;
+    this.offsetY = 0;
+    this.handleSize = 14;
+  }
+
+  display() {
+    image(this.img, this.x, this.y, this.w, this.h);
+
+    // outline
+    noFill();
+    stroke(100);
+    strokeWeight(1);
+    rect(this.x, this.y, this.w, this.h);
+
+    // resize handle (bottom-right corner)
+    let hx = this.x + this.w;
+    let hy = this.y + this.h;
+  }
+
+  isMouseOver() {
+    return mouseX >= this.x && mouseX <= this.x + this.w &&
+           mouseY >= this.y && mouseY <= this.y + this.h;
+  }
+
+  isOverResizeHandle() {
+    let hx = this.x + this.w;
+    let hy = this.y + this.h;
+    return mouseX >= hx - this.handleSize && mouseX <= hx &&
+           mouseY >= hy - this.handleSize && mouseY <= hy;
+  }
+
+  startDrag() {
+    // drag only if over the image but not on the resize handle
+    if (this.isMouseOver() && !this.isOverResizeHandle()) {
+      this.dragging = true;
+      this.offsetX = this.x - mouseX;
+      this.offsetY = this.y - mouseY;
+      return true;
+    }
+    return false;
+  }
+
+  startResize() {
+    if (this.isOverResizeHandle()) {
+      this.resizing = true;
+      return true;
+    }
+    return false;
+  }
+
+  drag() {
+    if (this.dragging) {
+      this.x = mouseX + this.offsetX;
+      this.y = mouseY + this.offsetY;
+    }
+  }
+
+  resize() {
+    if (this.resizing) {
+      // simple resize from top-left corner, based on mouse position
+      this.w = max(20, mouseX - this.x);
+      this.h = max(20, mouseY - this.y);
+    }
+  }
+
+  stopInteraction() {
+    this.dragging = false;
+    this.resizing = false;
+  }
+}
+
+// =====================
+// Setup
+// =====================
 function setup() {
   canvasWidth = windowWidth;
   canvasHeight = windowHeight;
@@ -99,6 +192,10 @@ function setup() {
   scrapbookHeight = 600;
   scrapbookX = (canvasWidth - scrapbookWidth) / 2;
   scrapbookY = toolbarHeight + 40;
+  
+  // Hidden file input for images
+  imageInput = createFileInput(handleImageFile);
+  imageInput.hide();
   
   // Create toolbar buttons
   setupToolbarButtons();
@@ -134,6 +231,11 @@ function setupToolbarButtons() {
   addImageBtn.style('border', 'none');
   addImageBtn.style('border-radius', '5px');
   addImageBtn.style('cursor', 'pointer');
+  addImageBtn.mousePressed(() => {
+    // trigger hidden file input
+    imageInput.elt.value = ''; // clear previous selection
+    imageInput.elt.click();
+  });
   
   // Add Shape button
   addShapeBtn = createButton('Add Shape');
@@ -187,6 +289,31 @@ function setupToolbarButtons() {
   colorPicker.style('cursor', 'pointer');
 }
 
+// =====================
+// File handler for images
+// =====================
+function handleImageFile(file) {
+  if (file.type === 'image') {
+    loadImage(file.data, (img) => {
+      // Scale image to fit nicely inside scrapbook
+      let maxW = scrapbookWidth * 0.5;
+      let maxH = scrapbookHeight * 0.5;
+      let scale = min(maxW / img.width, maxH / img.height, 1);
+      let w = img.width * scale;
+      let h = img.height * scale;
+
+      let x = scrapbookX + (scrapbookWidth - w) / 2;
+      let y = scrapbookY + (scrapbookHeight - h) / 2;
+
+      let newImgEl = new ImageElement(img, x, y, w, h);
+      imageElements.push(newImgEl);
+    });
+  }
+}
+
+// =====================
+// Draw
+// =====================
 function draw() {
   background(220);
   
@@ -229,6 +356,11 @@ function drawScrapbook() {
       point(x, y);
     }
   }
+
+  // Draw images first so text can sit on top if you want
+  for (let imgEl of imageElements) {
+    imgEl.display();
+  }
   
   // Draw text elements
   for (let textEl of textElements) {
@@ -253,6 +385,9 @@ function drawScrapbook() {
   pop();
 }
 
+// =====================
+// Keyboard
+// =====================
 function keyPressed() {
   if (typingMode) {
     if (keyCode === ENTER) {
@@ -278,9 +413,8 @@ function keyPressed() {
     } else if (keyCode === BACKSPACE) {
       // Remove last character
       currentText = currentText.slice(0, -1);
-      return false; // Prevent default backspace behavior
+      return false;
     } else if (keyCode === ESCAPE) {
-      // Cancel typing
       typingMode = false;
       currentText = '';
       addTextBtn.style('background-color', '#ddd');
@@ -292,8 +426,10 @@ function keyPressed() {
   }
 }
 
+// =====================
+// Mouse interactions
+// =====================
 function mousePressed() {
-  // If in typing mode, set text position
   if (typingMode && mouseX >= scrapbookX && mouseX <= scrapbookX + scrapbookWidth &&
       mouseY >= scrapbookY && mouseY <= scrapbookY + scrapbookHeight) {
     typingX = mouseX;
@@ -301,8 +437,17 @@ function mousePressed() {
     return;
   }
   
-  // Check if clicking on a text element (only if not in typing mode)
   if (!typingMode) {
+    for (let i = imageElements.length - 1; i >= 0; i--) {
+      let imgEl = imageElements[i];
+      if (imgEl.startResize() || imgEl.startDrag()) {
+        selectedImage = imgEl;
+        imageElements.splice(i, 1);
+        imageElements.push(imgEl);
+        return;
+      }
+    }
+
     for (let i = textElements.length - 1; i >= 0; i--) {
       if (textElements[i].startDrag()) {
         selectedText = textElements[i];
@@ -316,12 +461,22 @@ function mousePressed() {
 }
 
 function mouseDragged() {
-  if (selectedText) {
+  if (selectedImage) {
+    if (selectedImage.resizing) {
+      selectedImage.resize();
+    } else {
+      selectedImage.drag();
+    }
+  } else if (selectedText) {
     selectedText.drag();
   }
 }
 
 function mouseReleased() {
+  if (selectedImage) {
+    selectedImage.stopInteraction();
+    selectedImage = null;
+  }
   if (selectedText) {
     selectedText.stopDrag();
     selectedText = null;
